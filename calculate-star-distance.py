7829d7233733de9c4087a3d6445efbc0fb0ef676
Cafalump <calumbucknall@hotmail.co.uk>
2017-10-05 11:36:48 +0100

    rewrote calculate-star-distance.py

--- calculate-star-distance.py
+++ calculate-star-distance.py
@@ -9,24 +9,33 @@ def polarToCartesian(ra, dec, dist, name="star"):
     Y = ((C * math.cos(B)) * math.sin(A))
     Z = C * math.sin(B)
 
-    return [X, Y, Z, name]
+    return { "name":name, "X":X, "Y":Y, "Z":Z }
 
-def takeAndConvertInputs():
+def convertInputsToCartesian(star):
+    ra = re.findall("[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?", star["raw_ra"])
+    dec = re.findall("[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?", star["raw_dec"])
+    dist = re.findall("[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?", star["raw_dist"])
+
+    return polarToCartesian(ra, dec, dist, star["name"])
+
+def takeInputs():
     name = input("Enter name of object: ")
     raw_ra = input("Enter right ascension: ")
     raw_dec = input("Enter declination: ")
     raw_dist = input("Enter distance in lightyears: ")
 
-    ra = re.findall("[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?", raw_ra)
-    dec = re.findall("[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?", raw_dec)
-    dist = re.findall("[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?", raw_dist)
+    return { "name":name, "raw_ra":raw_ra, "raw_dec":raw_dec, "raw_dist":raw_dist }
 
-    cartesianCoords = polarToCartesian(ra, dec, dist, name)
-    return cartesianCoords
+def calculateStarDistance(first, second, output=True):
+    finaldistance = math.sqrt( (first["X"] - second["X"])**2 + (first["Y"] - second["Y"])**2 + (first["Z"] - second["Z"])**2 )
+    if output:
+        print("The distance between", first["name"], "and", second["name"], "is", finaldistance, "ly")
+    return finaldistance
 
-firstobject = takeAndConvertInputs()
-secondobject = takeAndConvertInputs()
+first = takeInputs()
+second = takeInputs()
+calculateStarDistance(convertInputsToCartesian(first), convertInputsToCartesian(second))
 
-finaldistance = math.sqrt( ((firstobject[0] - secondobject[0])**2) + ((firstobject[1] - secondobject[1])**2) + ((firstobject[2] - secondobject[2])**2) )
+#finaldistance = math.sqrt( ((firstobject[0] - secondobject[0])**2) + ((firstobject[1] - secondobject[1])**2) + ((firstobject[2] - secondobject[2])**2) )
 
-print("The distance between", firstobject[3], "and", secondobject[3], "is", finaldistance, "ly")
+#print("The distance between", firstobject[3], "and", secondobject[3], "is", finaldistance, "ly")
