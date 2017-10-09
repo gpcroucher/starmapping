215dc6a9e261ebdb1c0507da6df16e560f551fbc
George Croucher <gpcroucher@gmail.com>
2017-10-09 14:41:07 +0100

    wrote filter script

--- readGlieseCatalog.py
+++ readGlieseCatalog.py
@@ -1,5 +1,23 @@
-include calculate-star-distance.py
+#import calculate-star-distance.py as calc
+import csv
 
-with open(...) as f:
-    for line in f:
-#        <do something with line>
+# base = input("Enter name of star: ")
+
+working_list = []
+fieldnames = []
+
+with open("hygdata_v3.csv") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        if "I" in row["spect"]:
+            working_list.append(row)
+    fieldnames = reader.fieldnames
+
+#print(working_list)
+
+with open("output.csv", "w") as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames)
+    writer.writeheader()
+    writer.writerows(working_list)
+
+# with open("output.csv"
